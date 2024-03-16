05700ad24065a0a7e6b8f7f7230fc87ff5dd3703
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2024-03-16 15:06:51 -0500

    add comment to selectors file

--- src/app/state/books.selectors.ts
+++ src/app/state/books.selectors.ts
@@ -1,12 +1,13 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { Book } from '../book-list/books.model';
 
+//contain book list from google (calling from app.component.ts ngOnInit)
 export const selectBooks = createFeatureSelector<ReadonlyArray<Book>>('books');
 
-export const selectCollectionState = createFeatureSelector<
-  ReadonlyArray<string>
->('collection');
+//contain bookid collection from user (calling from AddBook button)
+export const selectCollectionState = createFeatureSelector<ReadonlyArray<string>>('collection');
 
+//return book from bookid collection from user base on book list from google
 export const selectBookCollection = createSelector(
   selectBooks,
   selectCollectionState,
